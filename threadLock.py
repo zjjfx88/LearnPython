0e6c237fd0f224df18b7b656c80ca3ec3e86b83f
zjjfx88 <zjj7842806@163.com>
2016-08-05 17:05:21 +0800

    add

--- threadLock.py
+++ threadLock.py
@@ -4,6 +4,7 @@ import time, threading
 
 #假定这是你的银行存款
 balance = 0
+lock = threading.Lock()
 
 def change_it(n):
     #先存后取结果应该为0
@@ -13,7 +14,11 @@ def change_it(n):
 
 def run_thread(n):
     for i in range(100000):
-        change_it(n)
+        lock.acquire()
+        try:
+            change_it(n)
+        finally:
+            lock.release
 
 t1 = threading.Thread(target=run_thread, args=(5,))
 t2 = threading.Thread(target=run_thread, args=(3,))
