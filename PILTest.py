7d8df7ca78a17e8f35b24dd5905137cba00388b5
zjjfx88 <zjj7842806@163.com>
2016-08-11 10:43:23 +0800

    2016-08-11

--- PILTest.py
+++ PILTest.py
@@ -1,10 +1,14 @@
 # -*- coding:utf-8 -*-
-from PIL import Image
+from PIL import Image,ImageFilter
 # 打开一个jpg图像文件，注意路径要改成你自己的:
 im = Image.open('DSC_4245.jpg')
 # 获得图像尺寸:
-w, h = im.size
+#w, h = im.size
 # 缩放到50%:
-im.thumbnail((w//2, h//2))
+#im.thumbnail((w//2, h//2))
 # 把缩放后的图像用jpeg格式保存:
-im.save('DSC_4245.jpg','jpeg')
+#im.save('thumbnail.jpg','jpeg')
+
+im2 = im.filter(ImageFilter.BLUR)
+im2.save('blur.jpg','jpeg')
+
