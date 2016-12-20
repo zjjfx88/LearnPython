e3505c6989d637ba1f09e7724ea79a7293f8a00b
zjjfx88 <zjj7842806@163.com>
2016-12-20 19:11:41 +0800

    add by 2016-12-20

--- Flask_App.py
+++ Flask_App.py
@@ -1,3 +1,27 @@
 # -*- coding:utf-8 -*-
 
-from flask
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    return '<h1>Home</h>'
+
+@app.route('/signin',methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">登陆</button></p>
+              </form>'''
+@app.route('/signin',methods=['POST'])
+def singin():
+    # 需要从request对象读取表单内容：
+    if request.form['username']=='admin' and request.form['password']=='password':
+        return '<h3>Hello,admin!</h3>'
+    return '<h3>Bad user or password</h3>'
+
+if __name__ == '__main__':
+    app.run()
